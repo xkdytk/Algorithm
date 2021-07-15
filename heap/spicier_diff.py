5a634a4249f294d6536a164cd0eeefed63ea222c
xkdytk <xkdytk@gmail.com>
2021-07-15 22:10:07 +0900

    heap update

---
+++ heap/spicier_diff.py
@@ -0,0 +1,19 @@
+import heapq as hq
+
+def solution(scoville, K):
+
+    hq.heapify(scoville)
+    answer = 0
+    while True:
+        first = hq.heappop(scoville)
+        if first >= K:
+            break
+        if len(scoville) == 0:
+            return -1
+        second = hq.heappop(scoville)
+        hq.heappush(scoville, first + second*2)
+        answer += 1  
+
+    return answer
+
+# 풀이가 비슷하지만 다른 형태로 구현 (while 안에 if문으로 구현)
