66b541aa3f909229c46b9e63f0c0a274c6ad757d
xkdytk <xkdytk@gmail.com>
2021-08-16 23:14:26 +0900

    DFS_BFS update

---
+++ DFS_BFS/target_num.py
@@ -0,0 +1,15 @@
+from collections import deque
+
+def solution(numbers, target):
+    answer = 0
+    queue = deque([(0, 0)])
+    while queue:
+        sum, idx = queue.popleft()
+        if idx > len(numbers):
+            break
+        elif idx == len(numbers) and sum == target:
+            answer += 1
+        queue.append((sum+numbers[idx-1], idx+1))
+        queue.append((sum-numbers[idx-1], idx+1))
+
+    return answer
