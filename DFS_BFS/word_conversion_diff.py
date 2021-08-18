3417d360f3c039c9c920a3e8ce1f2054aefefe38
xkdytk <xkdytk@gmail.com>
2021-08-18 16:35:43 +0900

    DFS_BFS update

--- DFS_BFS/word_conversion_diff.py
+++ DFS_BFS/word_conversion_diff.py
@@ -2,7 +2,7 @@ def dfs(begin, target, words, visit):
     answer = 0
     stacks = [begin]
     while stacks:
-        stack = stacks.pop()
+        stack = stacks.pop() # 리스트의 마지막 부분을 뺌 (DFS)
         if stack == target:
             return answer
         
@@ -19,7 +19,7 @@ def dfs(begin, target, words, visit):
 def solution(begin, target, words):
     if target not in words:
         return 0
-    visit = [0 for i in words]
+    visit = [0 for i in words] # 깊이 우선 탐색이기 때문에 방문 기록을 남겨야함
     answer = dfs(begin, target, words, visit)
 
     return answer
